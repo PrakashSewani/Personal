f080e6ee6610bb4375bda998c3bf0cd1efe4f857
PrakashSewani <adamjones2227@gmail.com>
2022-02-13 23:18:43 +0530

    no idea

---
+++ Projects/MajorProject/ChatImplementation/APIANDNLP/nlp.py
@@ -0,0 +1,9 @@
+from rake_nltk import Rake
+
+rake_nltk_var=Rake()
+
+while(True):
+    text=input()
+    rake_nltk_var.extract_keywords_from_text(text)
+    keyword_extracted=rake_nltk_var.get_ranked_phrases()
+    print(keyword_extracted)
