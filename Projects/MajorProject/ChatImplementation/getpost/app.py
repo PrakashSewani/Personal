6493aa03b6ecfee8c6f65c016bef5a6e47db2719
PrakashSewani <adamjones2227@gmail.com>
2022-03-27 17:54:44 +0530

    major work

--- Projects/MajorProject/ChatImplementation/getpost/app.py
+++ Projects/MajorProject/ChatImplementation/getpost/app.py
@@ -10,6 +10,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
+    print(userText)
     # return chatbot_response(userText)
     return str(input())
 
